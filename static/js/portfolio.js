6949722d43a38e73fec7c0b5d4eee17089c393ab
Kenneth Tuten <ktuten@fb.com>
2018-12-22 16:14:41 -0600

    added new chart for days

--- static/js/portfolio.js
+++ static/js/portfolio.js
@@ -1,3 +1,4 @@
+
 (function($) {
 	"use strict"; // Start of use strict
 
@@ -44,6 +45,16 @@
 	// });
 
 })(jQuery); // End of use strict
+
+window.chartColors = {
+	red: 'rgb(255, 99, 132)',
+	orange: 'rgb(255, 159, 64)',
+	yellow: 'rgb(255, 205, 86)',
+	green: 'rgb(75, 192, 192)',
+	blue: 'rgb(54, 162, 235)',
+	purple: 'rgb(153, 102, 255)',
+	grey: 'rgb(201, 203, 207)'
+};
 
 // made global incase window is resized
 var DATA;
